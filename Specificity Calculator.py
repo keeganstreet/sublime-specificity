c2421a7fffb8236332305597e143eb2e44f1abf7
keeganstreet <keeganstreet@gmail.com>
2012-09-23 20:47:34 +1000

    Wrote JavaScript app for accepting input from Sublime Text, generating the result and returning the output

--- Specificity Calculator.py
+++ Specificity Calculator.py
@@ -11,20 +11,23 @@ class SpecificityCalculatorCommand(sublime_plugin.TextCommand):
 			text += self.view.substr(line) + '\n'
 
 		# Send the text to the specificity.js Node app
-		cmd = [
+		# Do this by opening a subprocess in a new thread
+		args = [
 			sublime.load_settings(__name__ + '.sublime-settings').get('node_path'),
 			sublime.packages_path() + '/' + __name__ + '/lib/specificity.js',
 			'--selectors', text
 		]
-		threading.Thread(target = _run, args = cmd).start()
+		threading.Thread(target = _run, args = [args]).start()
 
-def _run(cmd, args):
+def _run(args):
 
 	try:
-		process = subprocess.Popen([cmd, args],
+		process = subprocess.Popen(args,
 			stdin = subprocess.PIPE,
 			stdout = subprocess.PIPE,
 			stderr = subprocess.STDOUT)
+
+		# Show the results in the output panel
 		print process.communicate()[0]
 
 	except OSError:
