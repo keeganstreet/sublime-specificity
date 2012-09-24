4ed1245ccfe1dd91ddae66331be962d6f1830eb9
keeganstreet <keeganstreet@gmail.com>
2012-09-24 20:52:38 +1000

    Updated Python app to display output to user

--- Specificity Calculator.py
+++ Specificity Calculator.py
@@ -17,18 +17,42 @@ class SpecificityCalculatorCommand(sublime_plugin.TextCommand):
 			sublime.packages_path() + '/' + __name__ + '/lib/specificity.js',
 			'--selectors', text
 		]
-		threading.Thread(target = _run, args = [args]).start()
 
-def _run(args):
+		thread = NodeJS(args)
+		thread.start()
+		self.handle_thread(thread)
 
-	try:
-		process = subprocess.Popen(args,
-			stdin = subprocess.PIPE,
-			stdout = subprocess.PIPE,
-			stderr = subprocess.STDOUT)
+	def handle_thread(self, thread):
+		if (thread.isAlive()):
+			sublime.set_timeout(lambda: self.handle_thread(thread), 100)
+		else:
+			if (thread.result != False):
+				# Show the result in an output window
+				output_panel = self.view.window().get_output_panel("specificity")
+				edit = output_panel.begin_edit()
+				output_panel.insert(edit, output_panel.size(), thread.result)
+				output_panel.end_edit(edit)
+				self.view.window().run_command("show_panel", {"panel": "output.specificity"})
 
-		# Show the results in the output panel
-		print process.communicate()[0]
 
-	except OSError:
-		sublime.error_message('Error calling NodeJS app')
+class NodeJS(threading.Thread):
+
+	def __init__(self, args):
+		self.args = args
+		self.result = None
+		threading.Thread.__init__(self)
+
+	def run(self):
+		try:
+			process = subprocess.Popen(self.args,
+				stdin = subprocess.PIPE,
+				stdout = subprocess.PIPE,
+				stderr = subprocess.STDOUT)
+
+			# Make the result accessible by the main thread
+			self.result = process.communicate()[0]
+
+		except OSError:
+			sublime.error_message('Error calling NodeJS app')
+			self.result = False
+
